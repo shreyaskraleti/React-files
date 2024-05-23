91cf2d4e749db285cadf76d84cd3366b70483575
Shreyas <shreyas.kraleti2@gmail.com>
2024-05-23 13:37:21 +0530

    component files are added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React App</h1>
+      <Header/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
